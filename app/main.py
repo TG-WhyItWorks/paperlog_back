733ec5d877883ba2c584ad8d064090f61bed0542
kh <khjeon1222@gmail.com>
2025-07-25 18:07:07 +0900

    feat main

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-print("hello world")
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
+import os
+
+
+app = FastAPI()
+
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=os.getenv("secret_key")
+)
