7b0d2a2fe2b05feeca0c52c0decf68232ab2eb59
yulgim05 <yulgim2024@gmail.com>
2025-07-29 11:09:58 +0900

    revision of search/crud.py using async

--- app/core/search/crud.py
+++ app/core/search/crud.py
@@ -2,17 +2,19 @@
 # router.py에서 DB 코드를 반복하지 않도록 역할 분리용 -> router.py에서 import crud
 
 
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 from app.core.search.models import Paper
 from app.core.search.schemas import PaperCreate
 
-def create_paper(db: Session, paper: PaperCreate):
+async def create_paper(db: AsyncSession, paper: PaperCreate):
     db_paper = Paper(**paper.dict())
     db.add(db_paper)
-    db.commit()
-    db.refresh(db_paper)
+    await db.commit()
+    await db.refresh(db_paper)
     return db_paper
 
 
-def get_paper(db: Session, paper_id: int):
-    return db.query(Paper).filter(Paper.id == paper_id).first()
+async def get_paper(db: AsyncSession, paper_id: int):
+    result = await db.execute(select(Paper).filter(Paper.id == paper_id))
+    return result.scalar_one_or_none()
